3ac23f3bffd5aa872d5328bc0980237d5aa52243
LucasPendley <99683560+LucasPendley@users.noreply.github.com>
2022-02-17 14:21:47 -0600

    start exercise 1

---
+++ C02__JavaScript-Foundations-Part-1__JavaScript/~Finished Code/class 2 exercise 1.js
@@ -0,0 +1,18 @@
+// strings
+console.log("hello" === "hello")
+
+//numbers
+console.log("158" === "158")
+
+// booleans
+console.log(Boolean(undefined))
+
+// null
+console.log(null)
+
+// object
+console.log({ name: "Lucas" }.name == { name: "Lucas"}.name)
+
+//array
+console.log(["Hello", "goodbye"][0] == "Hello")
+
