90a2637cac6f8eb59d8d94f843adaf7665cae28b
LucasPendley <99683560+LucasPendley@users.noreply.github.com>
2022-03-10 14:46:31 -0600

    Create Exercise 1 class 8.js

---
+++ Exercise 1 class 8.js
@@ -0,0 +1,17 @@
+//Exercise 1 class 8
+//Create a function that takes a "base number" as an argument. 
+//This function should return another function that takes a new argument 
+//and returns the sum of the "base number" and the new argument.
+
+var exponent = function(a, n) 
+{
+    if (n === 0)
+        {
+         return 1;
+        }
+     else
+    {
+    return a * exponent(a, n-1)
+    }
+};
+console.log(exponent(4,2))
